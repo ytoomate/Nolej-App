a738135c1ee77b8fe277b6be1201b7f9ff21d9a5
ytoomate <nexus2450@gmail.com>
2023-10-27 20:30:21 +0600

    Commity

--- Nolej/ContentView.swift
+++ Nolej/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("OHAYO, ONICHAN")
+            Text("OHAYO, ONICHAN!")
         }
         .padding()
     }
