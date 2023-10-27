d005f60140d4ab27a2af3ea3899695b859566a0d
ytoomate <nexus2450@gmail.com>
2023-10-27 19:42:01 +0600

    Initial Commit

---
+++ Nolej/NolejApp.swift
@@ -0,0 +1,17 @@
+//
+//  NolejApp.swift
+//  Nolej
+//
+//  Created by niccolò machiavelli on 27.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct NolejApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
